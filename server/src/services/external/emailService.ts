995d47644bd2af5ab6b1dc41269d6011cdcc0b23
BertilF <bertilfrigaard@gmail.com>
2025-06-21 17:48:02 +0200

    changes email port to use 587

--- server/src/services/external/emailService.ts
+++ server/src/services/external/emailService.ts
@@ -3,8 +3,7 @@ import { logError } from "../../utils/logging";
 
 const transporter: Transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
